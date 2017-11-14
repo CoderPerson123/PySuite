85c52bc81677510040d8e031cb44f5d476f71856
Nathan R <NJR4123@gmail.com>
2017-11-14 16:28:31 -0500

    Update and rename PyCalc to PyCalc.py

---
+++ PySuite/PyCalc.py
@@ -0,0 +1,18 @@
+# PyCalc
+# Created by Nathan R (Mosrod) https://github.com/Mosrod
+
+# Imports
+from time import sleep
+import sys
+
+# Variables
+run = 1
+
+# Definitions
+
+# Add
+def add():
+  x = input("Num1: ")
+  y = input("Num2: ")
+  z = x+y
+  print(z)
